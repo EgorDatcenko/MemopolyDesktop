89b974ebc8e6fdd50ffa5217594d0eb2583dc2bf
Даценко Егор <egordatcenko7@gmail.com>
2026-05-31 21:29:17 +0300

    Defer screen transitions to avoid menu flicker

--- core/src/main/java/com/memopoly/Memopoly.java
+++ core/src/main/java/com/memopoly/Memopoly.java
@@ -2,7 +2,14 @@ package com.memopoly;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.kotcrab.vis.ui.VisUI;
 import com.memopoly.Screens.*;
 import com.memopoly.game.model.GameState;
@@ -12,6 +19,8 @@ import com.memopoly.network.NetworkListener;
 import com.memopoly.network.packets.RollDiceResponse;
 import com.memopoly.network.packets.StartGameRequest;
 import com.memopoly.utils.LanguageManager;
+
+import java.util.function.Supplier;
 
 public class Memopoly extends Game implements NetworkListener {
     private static final String SETTINGS_PREFS = "memopoly-settings";
@@ -27,6 +36,8 @@ public class Memopoly extends Game implements NetworkListener {
     private volatile GameState latestGameState;
     private boolean lobbyOpened;
     private LanguageManager languageManager;
+    private BitmapFont localizedUiFont;
+    private int pendingScreenTransitionId;
 
     @Override
     public void create() {
@@ -35,6 +46,7 @@ public class Memopoly extends Game implements NetworkListener {
         batch = new SpriteBatch();
         gameClient = new GameClient(this);
         languageManager = new LanguageManager(getSettingsPreferences());
+        applyLocalizedFonts();
         applySettings(
             Gdx.app.getPreferences(SETTINGS_PREFS).getFloat("music_volume", 0.7f),
             Gdx.app.getPreferences(SETTINGS_PREFS).getFloat("sfx_volume", 0.85f),
@@ -105,20 +117,20 @@ public class Memopoly extends Game implements NetworkListener {
     }
 
     public void openLobby() {
-        screenManager.set(new LobbyScreen(this));
+        scheduleScreenTransition(() -> new LobbyScreen(this));
     }
 
     public void openMenu() {
-        screenManager.set(new MainMenuScreen(this));
+        scheduleScreenTransition(() -> new MainMenuScreen(this));
     }
     public void openSettings() {
-        screenManager.set(new SettingsScreen(this));
+        scheduleScreenTransition(() -> new SettingsScreen(this));
     }
     public void openGame() {
-        screenManager.set(new GameScreen(this));
+        scheduleScreenTransition(() -> new GameScreen(this));
     }
     public void openGameLoading() {
-        screenManager.set(new LoadingScreen(this, "Загрузка матча", () -> new GameScreen(this)));
+        scheduleScreenTransition(() -> new LoadingScreen(this, "Загрузка матча", () -> new GameScreen(this)));
     }
     public void leaveRoomToMenu() {
         if (gameServer != null) {
@@ -177,6 +189,17 @@ public class Memopoly extends Game implements NetworkListener {
         return languageManager;
     }
 
+    private void scheduleScreenTransition(Supplier<BaseScreen> screenSupplier) {
+        int transitionId = ++pendingScreenTransitionId;
+        Gdx.app.postRunnable(() -> {
+            if (transitionId != pendingScreenTransitionId) {
+                return;
+            }
+            applyLocalizedFonts();
+            screenManager.set(screenSupplier.get());
+        });
+    }
+
     public void applySettings(float musicVolume, float sfxVolume, boolean fullscreen) {
         com.badlogic.gdx.Preferences preferences = getSettingsPreferences();
         preferences.putFloat("music_volume", musicVolume);
@@ -192,9 +215,73 @@ public class Memopoly extends Game implements NetworkListener {
         }
     }
 
+    private void applyLocalizedFonts() {
+        String fontFolderPath = languageManager.getLanguage() == LanguageManager.Language.RU ? "fonts_ru" : "fonts_en";
+        BitmapFont newFont = tryLoadBitmapFont(fontFolderPath);
+        if (newFont == null) {
+            newFont = tryGenerateFontFromTtf(fontFolderPath);
+        }
+        if (newFont == null) {
+            Gdx.app.log("Fonts", "No font in " + fontFolderPath + " (.fnt/.ttf/.otf). Keep default VisUI font.");
+            return;
+        }
+        if (localizedUiFont != null) {
+            localizedUiFont.dispose();
+        }
+        localizedUiFont = newFont;
+
+        Label.LabelStyle labelStyle = VisUI.getSkin().get(Label.LabelStyle.class);
+        labelStyle.font = localizedUiFont;
+        TextButton.TextButtonStyle textButtonStyle = VisUI.getSkin().get(TextButton.TextButtonStyle.class);
+        textButtonStyle.font = localizedUiFont;
+        TextField.TextFieldStyle textFieldStyle = VisUI.getSkin().get(TextField.TextFieldStyle.class);
+        textFieldStyle.font = localizedUiFont;
+        CheckBox.CheckBoxStyle checkBoxStyle = VisUI.getSkin().get(CheckBox.CheckBoxStyle.class);
+        checkBoxStyle.font = localizedUiFont;
+    }
+
+    private BitmapFont tryLoadBitmapFont(String folderPath) {
+        FileHandle folder = Gdx.files.internal(folderPath);
+        if (!folder.exists() || !folder.isDirectory()) {
+            return null;
+        }
+        for (FileHandle file : folder.list()) {
+            if ("fnt".equalsIgnoreCase(file.extension())) {
+                return new BitmapFont(file, false);
+            }
+        }
+        return null;
+    }
+
+    private BitmapFont tryGenerateFontFromTtf(String folderPath) {
+        FileHandle folder = Gdx.files.internal(folderPath);
+        if (!folder.exists() || !folder.isDirectory()) {
+            return null;
+        }
+        for (FileHandle file : folder.list()) {
+            String ext = file.extension().toLowerCase();
+            if (!"ttf".equals(ext) && !"otf".equals(ext)) {
+                continue;
+            }
+            FreeTypeFontGenerator generator = new FreeTypeFontGenerator(file);
+            FreeTypeFontGenerator.FreeTypeFontParameter param = new FreeTypeFontGenerator.FreeTypeFontParameter();
+            param.size = 30;
+            param.characters = FreeTypeFontGenerator.DEFAULT_CHARS
+                + "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+                + "абвгдеёжзийклмнопрстуфхцчшщъыьэюя№";
+            BitmapFont font = generator.generateFont(param);
+            generator.dispose();
+            return font;
+        }
+        return null;
+    }
+
     @Override
     public void dispose() {
         batch.dispose();
+        if (localizedUiFont != null) {
+            localizedUiFont.dispose();
+        }
         VisUI.dispose();
         if (gameServer != null) {
             gameServer.stop();
